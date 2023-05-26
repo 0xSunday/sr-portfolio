a54e62e5bbdb6fb014096c13445e88ed8bf1cf63
0xSunil <sunday7637@gmail.com>
2023-05-27 00:12:14 +0530

    about

--- app/page.js
+++ app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
           <Hero />
         </div>
 
-        {/* <About /> */}
+        <About />
         {/* <Skills /> */}
         {/* <Projects /> */}
         {/* <Experience /> */}
